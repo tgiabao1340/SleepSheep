1db95d7ae6726bae62014821c52a372aa9ffe861
tgiabao1340 <39157595+tgiabao1340@users.noreply.github.com>
2019-10-09 20:57:56 +0700

    add diaglog, slide, background

--- app/src/main/java/com/example/sleepee/SleepActivity.java
+++ app/src/main/java/com/example/sleepee/SleepActivity.java
@@ -2,20 +2,37 @@ package com.example.sleepee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.sleepee.model.Sleep;
+import com.ncorti.slidetoact.SlideToActView;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
+import java.util.List;
 
 public class SleepActivity extends AppCompatActivity {
     private TextView textView_time;
+    private SlideToActView slideToActStop;
     private final int TIMEOFCYCLE = 90; // 90 min
+    private final int MAXCYCLE = 5;
+    private final int MINCYCLE = 3;
     private int TIMETOFALLASLEEP = 14; // 14 min
     private long START_TIME,END_TIME,TOTAL_TIME;
     @Override
@@ -24,34 +41,94 @@ public class SleepActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sleep);
         textView_time = findViewById(R.id.textView_time);
         Intent intent = getIntent();
-        if(intent!=null){
-            if(intent.hasExtra("START_TIME") && intent.hasExtra("END_TIME")){
-                START_TIME = intent.getLongExtra("START_TIME",0);
-                END_TIME = intent.getLongExtra("END_TIME",0);
-                Calendar cal = Calendar.getInstance();
-                if((new Date(START_TIME).compareTo(new Date(END_TIME)))>0){
-                    cal.setTimeInMillis(END_TIME);
-                    cal.add(Calendar.DATE,1);
-                    END_TIME = cal.getTimeInMillis();
+        if(intent!=null) if (intent.hasExtra("START_TIME") && intent.hasExtra("END_TIME")) {
+            START_TIME = intent.getLongExtra("START_TIME", 0);
+            END_TIME = intent.getLongExtra("END_TIME", 0);
+            Calendar cal = Calendar.getInstance();
+            if ((new Date(START_TIME).compareTo(new Date(END_TIME))) > 0) {
+                cal.setTimeInMillis(END_TIME);
+                cal.add(Calendar.DATE, 1);
+                END_TIME = cal.getTimeInMillis();
+            }
+            TOTAL_TIME = Math.abs(END_TIME - START_TIME - TIMETOFALLASLEEP * 60 * 1000);
+            Calendar calStartTime = Calendar.getInstance();
+            Calendar calEndTime = Calendar.getInstance();
+            Calendar calTimeMin = Calendar.getInstance();
+            Calendar calWakeTime = Calendar.getInstance();
+            calStartTime.setTimeInMillis(START_TIME);
+            calEndTime.setTimeInMillis(END_TIME);
+            calTimeMin.setTimeInMillis(END_TIME);
+            calWakeTime.setTimeInMillis(timeWake(START_TIME, calculateCycle(TOTAL_TIME)));
+            calTimeMin.add(Calendar.MINUTE, -30);
+            DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+            if (calculateCycle(TOTAL_TIME) < MINCYCLE || calculateCycle(TOTAL_TIME) > MAXCYCLE) {
+                List<Long> list = new ArrayList<>();
+                for (int i = MINCYCLE; i < MAXCYCLE + 1; i++) {
+                    list.add(timeWake(START_TIME, i));
                 }
-                TOTAL_TIME = Math.abs(END_TIME - START_TIME - TIMETOFALLASLEEP*60*1000);
-                Calendar calEndTime = Calendar.getInstance();
-                Calendar calTimeMin = calEndTime.getInstance();
-                Calendar calWakeTime = Calendar.getInstance();
-                calEndTime.setTimeInMillis(END_TIME);
-                calTimeMin.setTimeInMillis(END_TIME);
-                calWakeTime.setTimeInMillis(timeWake(START_TIME,calculateCycle(TOTAL_TIME)));
-                calTimeMin.add(Calendar.MINUTE,-30);
-                DateFormat dateFormat = new SimpleDateFormat("HH:mm");
-                textView_time.setText("Alarm : " + dateFormat.format(calTimeMin.getTime())+ " - "+ dateFormat.format(calEndTime.getTime()));
-                //textView_time.setText((TOTAL_TIME/1000)/60 + "min| " + calculateCycle(TOTAL_TIME) +" | " + new Date(timeWake(START_TIME,calculateCycle(TOTAL_TIME))));
+                showDialog(list, END_TIME);
+            } else {
+                Calendar timeforAlarm = Calendar.getInstance();
+                timeforAlarm.setTimeInMillis(timeWake(START_TIME, calculateCycle(TOTAL_TIME)));
+                textView_time.setText(dateFormat.format(timeforAlarm.getTime()));
             }
         }
+        slideToActStop = findViewById(R.id.slideActToStop);
+        slideToActStop.setOnSlideCompleteListener(new SlideToActView.OnSlideCompleteListener() {
+            @Override
+            public void onSlideComplete(SlideToActView slideToActView) {
+                Intent intent = new Intent(SleepActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+    public void setAlarm(Calendar calendar){
+        textView_time.setText(String.format("Alarm : %02d:%02d %s",calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),(calendar.get(Calendar.AM_PM)==0)?"AM":"PM"));
+    }
+    public void showDialog(final List<Long> list, final Long endTime){
+        final Dialog dialog = new Dialog(SleepActivity.this,R.style.DialogCustomTheme);
+        final List<String> listTime = new ArrayList<>();
+        final Calendar calendar = Calendar.getInstance();
+        for (int i = 0; i < list.size(); i ++){
+            calendar.setTimeInMillis(list.get(i));
+            listTime.add(String.format("%02d:%02d %s",calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),(calendar.get(Calendar.AM_PM)==0)?"AM":"PM"));
+        }
+        // dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.diaglog_sleep);
+        Button btndialog = dialog.findViewById(R.id.btndialog);
+        calendar.setTimeInMillis(endTime);
+        btndialog.setText(String.format("ALarm : %02d:%02d %s",calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),(calendar.get(Calendar.AM_PM)==0)?"AM":"PM"));
+        btndialog.setOnClickListener(new View.OnClickListener() {
+            //Chose ENDTIME
+            @Override
+            public void onClick(View v) {
+                Calendar selectedTime = Calendar.getInstance();
+                selectedTime.setTimeInMillis(endTime);
+                setAlarm(selectedTime);
+                dialog.dismiss();
+            }
+        });
+        ListView listView = dialog.findViewById(R.id.listview);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,R.layout.list_item, R.id.tv, listTime);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            //Chose SUGGESTTIME
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Calendar selectedTime = Calendar.getInstance();
+                selectedTime.setTimeInMillis(list.get(position));
+                setAlarm(selectedTime);
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
     public long timeWake(long startTime,int cycle){
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(startTime);
-        calendar.add(Calendar.MINUTE,TIMEOFCYCLE*cycle);
+        calendar.add(Calendar.MINUTE,TIMEOFCYCLE*cycle + TIMETOFALLASLEEP);
         return calendar.getTime().getTime();
     }
     public int calculateCycle(long timeSleep){
--- app/src/main/java/com/example/sleepee/fragment/TodayFragment.java
+++ app/src/main/java/com/example/sleepee/fragment/TodayFragment.java
@@ -24,7 +24,6 @@ import java.net.URL;
 
 public class TodayFragment extends Fragment {
     private TextView textView_today;
-    ProgressDialog pd;
     public TodayFragment() {
         // Required empty public constructor
     }
@@ -33,75 +32,7 @@ public class TodayFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_today, container, false);
         textView_today = view.findViewById(R.id.textView_today);
-        //new JsonTask().execute("https://api.openweathermap.org/data/2.5/weather?id=2172797&APPID=b5a75f6145f44beb9e3c6018e670b18d");
         return view;
-    }
-    private class JsonTask extends AsyncTask<String, String, String> {
-
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            pd = new ProgressDialog(getActivity());
-            pd.setMessage("Please wait");
-            pd.setCancelable(false);
-            pd.show();
-        }
-
-        protected String doInBackground(String... params) {
-
-
-            HttpURLConnection connection = null;
-            BufferedReader reader = null;
-
-            try {
-                URL url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-
-
-                InputStream stream = connection.getInputStream();
-
-                reader = new BufferedReader(new InputStreamReader(stream));
-
-                StringBuffer buffer = new StringBuffer();
-                String line = "";
-
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line+"\n");
-                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
-
-                }
-
-                return buffer.toString();
-
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            if (pd.isShowing()){
-                pd.dismiss();
-            }
-            textView_today.setText(result);
-        }
     }
 }
 
